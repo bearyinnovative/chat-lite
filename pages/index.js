269ada60e2440f63aad12601f2b470c5b2236443
haiyang <phycgp@gmail.com>
2021-03-26 17:16:35 +0800

    feat: add poc login

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,59 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useRef, useState, useCallback } from "react";
+import { useRouter } from 'next/router'
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const usernameRef = useRef();
+  const passRef = useRef();
+  const router = useRouter()
+
+  const [webBase, setWebBase] = useState("http://poc.saas.bearychat.com");
+  const [haloReg, setHaloReg] = useState("http://haloreg.saas.bearychat.com/get_halo_info");
+
+  const handleLogin = useCallback(async () => {
+    const username = usernameRef.current.value;
+    const pass = passRef?.current.value;
+    const response = await fetch(`${webBase}/api/sso/external-mock`, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username,
+        password: pass,
+      }),
+    });
+
+    const ticket = (await response.json()).result.ticket;
+    window.__ticket = ticket;
+    router.push('/chat')
+  }, [usernameRef, passRef]);
+
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <label>Web Base</label>
+      <input
+        className={styles.input}
+        value={webBase}
+        onChange={(e) => {
+          setWebBase(e.target.value);
+        }}
+      />
+      <label>Halo Registry</label>
+      <input
+        className={styles.input}
+        value={haloReg}
+        onChange={(e) => {
+          setHaloReg(e.target.value);
+        }}
+      />
+      <label>用户名</label>
+      <input className={styles.input} ref={usernameRef} />
+      <label>密码</label>
+      <input type="password" className={styles.input} ref={passRef} />
+      <button className={styles.input} onClick={handleLogin}>
+        登录
+      </button>
     </div>
-  )
+  );
 }
